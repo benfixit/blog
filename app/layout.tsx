49181aaeabb041c580784b6de52997658ce612e7
Chukwuemeka Inya <inyachukwuemeka1@gmail.com>
2025-11-15 00:53:39 +0100

    setup blog

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Footer } from "kedu";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -27,7 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main>
+          {/* <Nav /> */}
+          {children}
+          <Footer />
+        </main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,85 @@
 import Image from "next/image";
+import styles from "@/app/ui/home.module.css";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+      <>
+        <section className={styles.latest}>
+          <h2>Latest</h2>
+          <div>
+            <Image src="/babel_typescript.webp" width={400} height={300} alt="Latest blog post" />
+            <div>
+              <span>Date</span>
+              <h2>Setting up Babel and Typescript</h2>
+              <p>Typescript has it's own compiler but there are times when you might want to use a different compiler due to personal preference or project requirements. In this article, we will learn how to use a compiler like Babel to compile our Typescript file to Javascript so that our code can run on any browser whilst the function of the Typescript compiler `tsc` will be limited to type checking our code.</p>
+              <a href="">Read article</a>
+            </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+        </section>
+        <section className={styles.trending}>
+          <h2>Trending</h2>
+          <div>
+            <div>
+              <Image src="/babel_typescript.webp" width={300} height={300} alt="Latest blog post" />
+              <div>
+                <span>Date</span>
+                <h2>Setting up Babel and Typescript</h2>
+                <p>Typescript has it's own compiler but there are times when you might want to use a different compiler due to personal preference or project requirements. In this article, we will learn how to use a compiler like Babel to compile our Typescript file to Javascript so that our code can run on any browser whilst the function of the Typescript compiler `tsc` will be limited to type checking our code.</p>
+                <a href="">Read article</a>
+              </div>
+            </div>
+            <div>
+              <Image src="/babel_typescript.webp" width={300} height={300} alt="Latest blog post" />
+              <div>
+                <span>Date</span>
+                <h2>Setting up Babel and Typescript</h2>
+                <p>Typescript has it's own compiler but there are times when you might want to use a different compiler due to personal preference or project requirements. In this article, we will learn how to use a compiler like Babel to compile our Typescript file to Javascript so that our code can run on any browser whilst the function of the Typescript compiler `tsc` will be limited to type checking our code.</p>
+                <a href="">Read article</a>
+              </div>
+            </div>
+            <div>
+              <Image src="/babel_typescript.webp" width={300} height={300} alt="Latest blog post" />
+              <div>
+                <span>Date</span>
+                <h2>Setting up Babel and Typescript</h2>
+                <p>Typescript has it's own compiler but there are times when you might want to use a different compiler due to personal preference or project requirements. In this article, we will learn how to use a compiler like Babel to compile our Typescript file to Javascript so that our code can run on any browser whilst the function of the Typescript compiler `tsc` will be limited to type checking our code.</p>
+                <a href="">Read article</a>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className={styles.popular}>
+          <h2>Popular</h2>
+          <div>
+            <div>
+              <Image src="/babel_typescript.webp" width={350} height={350} alt="Latest blog post" />
+              <div>
+                <span>Date</span>
+                <h2>Setting up Babel and Typescript</h2>
+                <p>Typescript has it's own compiler but there are times when you might want to use a different compiler due to personal preference or project requirements. In this article, we will learn how to use a compiler like Babel to compile our Typescript file to Javascript so that our code can run on any browser whilst the function of the Typescript compiler `tsc` will be limited to type checking our code.</p>
+                <a href="">Read article</a>
+              </div>
+            </div>
+            <div>
+              <Image src="/babel_typescript.webp" width={350} height={350} alt="Latest blog post" />
+              <div>
+                <span>Date</span>
+                <h2>Setting up Babel and Typescript</h2>
+                <p>Typescript has it's own compiler but there are times when you might want to use a different compiler due to personal preference or project requirements. In this article, we will learn how to use a compiler like Babel to compile our Typescript file to Javascript so that our code can run on any browser whilst the function of the Typescript compiler `tsc` will be limited to type checking our code.</p>
+                <a href="">Read article</a>
+              </div>
+            </div>
+            <div>
+              <Image src="/babel_typescript.webp" width={350} height={350} alt="Latest blog post" />
+              <div>
+                <span>Date</span>
+                <h2>Setting up Babel and Typescript</h2>
+                <p>Typescript has it's own compiler but there are times when you might want to use a different compiler due to personal preference or project requirements. In this article, we will learn how to use a compiler like Babel to compile our Typescript file to Javascript so that our code can run on any browser whilst the function of the Typescript compiler `tsc` will be limited to type checking our code.</p>
+                <a href="">Read article</a>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
   );
 }
